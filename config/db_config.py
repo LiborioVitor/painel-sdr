62d2a3828cd1fb6c365ba90805036da9f1985e56
LiborioVitor <vitor.liborio@datasales.io>
2025-05-02 17:03:51 -0300

    Atualiza db_config - local e secrets

--- config/db_config.py
+++ config/db_config.py
@@ -1,12 +1,21 @@
-# Configurações da conexão ao banco
-from dotenv import load_dotenv
 import os
+import streamlit as st
 
-load_dotenv()
-
-DB_CONFIG = {
-    "user": os.getenv("DB_USER"),
-    "password": os.getenv("DB_PASSWORD"),
-    "host": os.getenv("DB_HOST"),
-    "database": os.getenv("DB_NAME")
-}
+if "DB_USER" in st.secrets:
+    # Produção (Streamlit Cloud)
+    DB_CONFIG = {
+        "user": st.secrets["DB_USER"],
+        "password": st.secrets["DB_PASSWORD"],
+        "host": st.secrets["DB_HOST"],
+        "database": st.secrets["DB_NAME"]
+    }
+else:
+    # Desenvolvimento local (.env)
+    from dotenv import load_dotenv
+    load_dotenv()
+    DB_CONFIG = {
+        "user": os.getenv("DB_USER"),
+        "password": os.getenv("DB_PASSWORD"),
+        "host": os.getenv("DB_HOST"),
+        "database": os.getenv("DB_NAME")
+    }
